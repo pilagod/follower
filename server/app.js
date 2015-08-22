159dd3be4b0f5f7aee7e9ba8958c18b22632f35e
pilagod <r03725029@ntu.edu.tw>
2015-08-22 15:44:27 +0800

    server side apis ver 1

--- server/app.js
+++ server/app.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    apis = require('./routes/apis');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var test = "test";
 var app = express();
 
 // view engine setup
@@ -17,13 +16,12 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/apis', apis);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
