7e901137c1dba7c1b0103afd0fb50278e0dddb85
wch4896 <wch4896@gmail.com>
2015-08-23 16:06:17 +0800

    - hackathon DEMO

--- www/js/map.js
+++ www/js/map.js
@@ -36,7 +36,16 @@ function initMap() {
         //     $('img[src^="https://graph.facebook.com"]').css({"border-radius": "80px"});
         //     // console.log($('img[src^="https://graph.facebook.com"]'));
         // });
-
+        // var pilagod = {lat:25.020058300000002, lng: 121.53760929999996},
+        //     yvonne = {lat:25.021258300000002, lng: 121.53500929999996};
+        //     count = 0;
+        addMarker_demo({lat:25.020058300000002, lng: 121.53760929999996}, "https://graph.facebook.com/867269946655421/picture?width=20");
+        addMarker_demo({lat:25.021258300000002, lng: 121.53500929999996}, "https://graph.facebook.com/10153403037175660/picture?width=20&height=20");
+        // while(count < 5){
+        //     addMarker_demo({lat:pilagod.lat + count * 0.01, lng: pilagod.lng + count * 0.01}, "https://graph.facebook.com/867269946655421/picture?width=20");
+        //     addMarker_demo({lat:yvonne.lat + count * 0.01, lng: yvonne.lng + count * 0.01}, "https://graph.facebook.com/867269946655421/picture?width=20");
+        // }
+        
     })
 }
 
@@ -47,6 +56,31 @@ function addMarker(location) {
         map: map,
         icon: {
             url: window.localStorage["picture"] + "?width=20"
+            //the size of the image is 32x32,
+            //when you want to add a border you must add 2*borderWidth to the size
+            // size:new google.maps.Size(50,50)
+        },
+        //define the shape
+        // shape:{coords:[17,17,18],type:'circle'},
+        //set optimized to false otherwise the marker  will be rendered via canvas
+        //and is not accessible via CSS
+        optimized: false
+    });
+    marker.iconLevel1 = window.localStorage["picture"] + "?width=20";
+    marker.iconLevel2 = window.localStorage["picture"] + "?width=35";
+
+    // markers.push(marker);
+    markers[window.localStorage["userid"]] = marker;
+}
+
+
+// Adds a marker to the map and push to the array.
+function addMarker_demo(location,url) {
+    var marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        icon: {
+            url: url
             //the size of the image is 32x32,
             //when you want to add a border you must add 2*borderWidth to the size
             // size:new google.maps.Size(50,50)
